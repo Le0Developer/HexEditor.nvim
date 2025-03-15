4b69b23773f9cdb0e11982229b1c9ce554997e70
Leo Developer <leo@git.leodev.win>
2025-03-15 15:36:12 +0100

    dont show cursor OOB or when hex edit is disabled

--- lua/HexEditor.lua
+++ lua/HexEditor.lua
@@ -10,20 +10,28 @@ local augroup_hex_editor = vim.api.nvim_create_augroup("hex_editor", { clear = t
 local calculate_text_position = function(row, col)
 	local text_row = row - 1
 	-- NOTE: Only move after space
+	if col < 10 then
+		return false, nil, nil
+	end
 	local text_col = 60 + math.floor((col - 13) / 3)
 	-- print(text_row, text_col)
-	return text_row, text_col
+	return true, text_row, text_col
 end
 
 local function highlight_corresponding_text()
 	-- Clear the previous highlight
 	vim.api.nvim_buf_clear_namespace(0, namespace_id, 0, -1)
+	if not vim.b.hex then
+		return
+	end
 	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 	-- Calculate the corresponding position in the text representation
 	-- This will depend on how your hex editor is structured
-	local text_row, text_col = calculate_text_position(row, col)
-	-- Highlight the corresponding position in the text representation
-	vim.api.nvim_buf_add_highlight(0, namespace_id, "HexEditorHighlight", text_row, text_col, text_col + 1)
+	local ok, text_row, text_col = calculate_text_position(row, col)
+	if ok then
+		-- Highlight the corresponding position in the text representation
+		vim.api.nvim_buf_add_highlight(0, namespace_id, "HexEditorHighlight", text_row, text_col, text_col + 1)
+	end
 end
 
 local function setup_highlight()
